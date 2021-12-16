e645153160f990f9c6d4ac646cfaf779df0005be
Jesmer <logtenbergjesmer@gmail.com>
2021-12-16 18:35:09 +0100

    Final Assignment

--- DocumentProcessor.py
+++ DocumentProcessor.py
@@ -10,18 +10,27 @@ class DocumentProcessor:
         ocrText = pytesseract \
             .image_to_string(Image.open(filename)) \
             .split('\n')
-        _lineWithContractNumber = self.__findLineContractNumber(ocrText)
-        _contractNumber =  self.__filterContractNumberFromLine(_lineWithContractNumber)
+        _lineWithContractNumber, type = self.__findLineContractNumber(ocrText)
+        _contractNumber =  self.__filterContractNumberFromLine(_lineWithContractNumber, type)
         return _contractNumber
 
     def __findLineContractNumber(self,stringList):
         print("fineLine")
+        contract_types = ["contractn", "leningn", "ingn"]
         for line in stringList:
-            if "Contractnummer" in line:
-                return line
+            for type in contract_types:
+                if type in line.lower():
+                    return line, type
+
         return "not found"
 
-    def __filterContractNumberFromLine(self,line):
-        line = line.replace("Contractnummer", "")
-        line = line.strip()
+    def __filterContractNumberFromLine(self,line, type):
+        contract_index = line.split()
+        for i in range (len(contract_index)):
+            if type in contract_index[i].lower():
+                if contract_index[i + 1][0] == '0':
+                    return contract_index[i + 1].replace("0", "O", 1)
+                elif contract_index[i + 1][0] == '2' and len(contract_index) < 9:
+                    return contract_index[i + 1].replace('2', 'Z', 1)
+                return contract_index[i + 1].strip()
         return line
